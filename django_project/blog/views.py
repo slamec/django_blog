b932339bf480fcd5afd0d73c27bcc2339459d791
Miroslav Kopecky <kopecky.mir@gmail.com>
2019-12-07 16:30:28 +0100

    add BASE_DIR - sidewide templates + showing path to template directory

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response, render
 import datetime
-from django.shortcuts import render
+from django.conf import settings
  
  
 def index(request):
-    return HttpResponse("Hello Django")
+    return HttpResponse("Hello I am Miro bro")
  
  
 def today_is(request):
-    now = datetime.datetime.now()    
-    return render(request, 'blog/datetime.html', {'now': now, 'template_name': 'blog/nav.html'})
+    now = datetime.datetime.now()
+    return render(request, 'blog/datetime.html', {
+                                    'now': now,
+                                    'template_name': 'blog/nav.html' ,
+                                    'base_dir': settings.BASE_DIR }
+                                )
