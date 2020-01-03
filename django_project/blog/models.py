86b013a93b7e41fdbd7d89b43d575bf9c1569b52
Miroslav Kopecky <kopecky.mir@gmail.com>
2020-01-03 20:36:42 +0100

    -

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -19,7 +19,7 @@ class Category(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
-    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE) #always must be on_delete!
 
 
 class Post(models.Model):
